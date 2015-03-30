61a87d2663d5ad08b4c8c2b68a774cda6ed09dc0
Kalpesh Kalekar <kalpeshk9967016292@gmail.com>
2015-03-30 11:51:18 +0530

    Responsive HP

--- index.php
+++ index.php
@@ -1,54 +1,106 @@
 <html>
-
 <head>
-<title>
-Hire a photographer.
-</title>
-<link rel="stylesheet" href="index.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="home.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="CSS/index.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<title>fotographer</title>
 </head>
 
-<body div class="body">
+<body>
 
-<div class="head">
-<div class="logo">
-<font color="#A79ECD">foto<span style="color: #7690C9;">grapher</span></font>
-</div>
-<div class="list">
-<ul>
-<li><a href="#">Work </a></li>
-<li><a href="#">Hire </a></li>
-<li><a href="#">Learn </a></li>
-<li><a href="#">Gallery </a></li>
-<li><a href="#">Login</a></li>
-</ul>
-</div>
+<div class="container-fluid ">
+	<div class="row top-buffer">
+	<div class="col-md-4 col-xs-12"><img src="img/logo.png" class="img-responsive center-block" width="400px"></div>
+	<div class="col-md-4 col-xs-0"></div>
+	<div class="col-md-1 hidden-xs hidden-sm "><div class="links btn">Work</div></div>
+	<div class="col-md-1 hidden-xs hidden-sm"><div class="links btn">Hire</div></div>
+	<div class="col-md-1 hidden-xs hidden-sm"><div class="links btn">Learn</div></div>
+	<div class="col-md-1 hidden-xs hidden-sm"><div class="links btn">Gallery</div></div>
+	</div>
 </div>
 
-<div class="mid">
-<div class="text">One stop for Professionals &amp; and newbies.
+<div class="container-fluid hidden-xs hidden-sm">
+	<div class="row">
+	<div class="col-md-12">
+	<div id="mycarousel" class="carousel slide top-buffer" data-ride="carousel">
+
+	<ol class="carousel-indicators">
+		<li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#mycarousel" data-slide-to="1"></li>
+		<li data-target="#mycarousel" data-slide-to="2"></li>
+		<li data-target="#mycarousel" data-slide-to="3"></li>
+	</ol>
+
+	<div class="carousel-inner" role="listbox">
+		<div class="item active">
+			<a href="#"><img src="img/Work.jpg" alt="hire"></a>
+			<div class="carousel-caption" style="color:black;">
+				<h3>Work</h3>
+				<p>Professional Photographers looking for work</p>
+			</div>
+		</div>
+
+		<div class="item">
+			<a href="#"><img src="img/hire.jpg" alt="hire"></a>
+			<div class="carousel-caption" style="color:black;">
+				<h3>Hire</h3>
+				<p>Looking for a professional photographer</p>
+			</div>
+		</div>		
+
+		<div class="item">
+			<a href="#"><img src="img/learn.jpg" alt="hire"></a>
+			<div class="carousel-caption">
+				<h3>Learn</h3>
+				<p>Learn Photography tips & Tricks from Professionals</p>
+			</div>
+		</div>	
+
+		<div class="item">
+			<a href="#"><img src="img/gallery.jpg" alt="hire"></a>
+			<div class="carousel-caption">
+				<h3>Gallery</h3>
+				<p>Browse Clicks by others</p>
+			</div>
+		</div>	
+	</div>
+
+	</div>
+	</div>
+	</div>
 </div>
+
+<div class="container-fluid hidden-lg hidden-md">
+	<div class="row top-buffer">
+		<div class="col-sm-6"><a href="#"><img src="img/worksm.jpg" width="300"> <h3>Work</h3> <p>I am a Photographer</p></div></a>
+		<div class="col-sm-6"><a href="#"><img src="img/hiresm.jpg" width="300"> <h3>Hire</h3> <p>I am Looking for Photographer</p></div></a>
+		<div class="col-sm-6"><a href="#"><img src="img/learnsm.jpg" width="300"> <h3>Learn</h3> <p>Photography tips & tricks</p></div></a>
+		<div class="col-sm-6"><a href="#"><img src="img/gallerysm.jpg" width="300"> <h3>Gallery</h3> <p>Clicks submitted by others.</p></div></a>
+	</div>
 </div>
 
-<div class="lower">
-
-<div class="lbs">
-<a href="#">Work
-<p>
-If you are a professional photographer.
-</p>
-</a>
-<a href="#">Hire</a>
-<a href="#">Learn</a>
-<a href="#">Gallery</a>
+<div class="contianer-fluid hidden-xs hidden-sm">
+	<div class="row top-buffer">
+		<div class="col-md-3 col-xs-6">
+			<h2>Work</h2><p>If you are a professional photographer.</p>
+		</div>
+		<div class="col-md-3 col-xs-6">
+			<h2>Hire</h2><p>Looking for a professional photographer.</p>
+		</div>
+		<div class="col-md-3 col-xs-6">
+			<h2>Learn</h2><p>If you want to learn photography.</p>
+		</div>
+		<div class="col-md-3 col-xs-6">
+			<h2>Gallery</h2><p>Brows through clicks uploaded by both professionals & learners.</p>
+		</div>
+	</div>
 </div>
 
 
-
+<script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
-
 </html>
